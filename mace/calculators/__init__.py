3aa6cc2eb122aa2f151039b5b490a0d77d8e6242
Hubert Beck <hubert.beck@matfyz.cuni.cz>
2026-06-23 14:19:43 +0200

    Add bias-only calculator for tests

--- mace/calculators/__init__.py
+++ mace/calculators/__init__.py
@@ -2,10 +2,12 @@ from .foundations_models import mace_anicc, mace_mp, mace_off
 from .lammps_mace import LAMMPS_MACE
 from .mace import MACECalculator
 from .mace_cg import MACECalculator_CG
+from .mace_cg_bias import MACECalculator_CG_Bias
 
 __all__ = [
     "MACECalculator",
     "MACECalculator_CG",
+    "MACECalculator_CG_Bias",
     "LAMMPS_MACE",
     "mace_mp",
     "mace_off",
